c4ed45a536eec91ef23b6d7e36474633bbeb6480
Andrew <afmcdnl@gmail.com>
2020-08-11 16:47:21 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    led.stopAnimation()
+    basic.clearScreen()
+    if (index < 6) {
+    	
+    } else {
+    	
+    }
+})
+let index = 0
+let womenInHistory = ["Ella Fitzgerald", "Pauline Johnson", "Indira Gandhi", "Katherine G. Johnson", "Germaine Greer", "Miriam Makeba", "Malala Yousafzai"]
+index = 0
+basic.forever(function () {
+	
+})
